fac28ab830f3f694bc838aee5b2737fafe6d4a82
ArijitMishra03 <arijitmishra090@gmail.com>
2025-05-09 21:48:34 +0530

    Update project1.js

--- project1/project1.js
+++ project1/project1.js
@@ -5,7 +5,7 @@ const inputBox=document.querySelector(".inputBox");
 const getMovieInfo=async (movie)=>{
     try{
     const myAPIKey="12138f51";
-    const url=`http://www.omdbapi.com/?apikey=${myAPIKey}&t=${movie}`;
+    const url=`https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?apikey=${myAPIKey}&t=${movie}`;
 
     
     const response=await fetch(url);
